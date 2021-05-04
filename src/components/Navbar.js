a05a01f0243662fbcd338e34fba89811d76c0166
sidonie <shelliednnguyen@gmail.com>
2021-05-04 09:33:09 -0700

    moved Navbar to center

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,35 +7,35 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const App = () => {
    return (
-      <div>
-         <ReactBS.Navbar className="container-fluid" bg="dark" variant="dark" expand="sm">
+      <div className="text-center bg-color">
+         <ReactBS.Row className="container-fluid justify-content-center" bg="dark" expand="sm">
             <Link to="/Welcome">
-               <ReactBS.Navbar href="#welcome">
-                  <Link to="/Welcome">
-                     <ReactBS.Nav.Link href="#welcome">
-                        <img src={logo} width="200" height="65" className="d-inline-block align-top" alt="Shellie Nguyen Logo" />
-                     </ReactBS.Nav.Link>
-                  </Link>
-               </ReactBS.Navbar>
+               <ReactBS.Nav.Link href="#welcome">
+                  <img src={logo} width="200" height="50" className="d-inline-block align-top" alt="Shellie Nguyen Logo" />
+               </ReactBS.Nav.Link>
             </Link>
-            <ReactBS.Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <ReactBS.Navbar.Collapse id="basic-navbar-nav">
-               <ReactBS.Nav className="ml-auto medium-font-size">
-                  <Link to="/About">
-                     <ReactBS.Nav.Link href="#about">About Me</ReactBS.Nav.Link>
-                  </Link>
-                  <Link to="/Portfolio">
-                     <ReactBS.Nav.Link href="#portfolio">Portfolio</ReactBS.Nav.Link>
-                  </Link>
-                  <Link to="/Resume">
-                     <ReactBS.Nav.Link href="#resume">Resume</ReactBS.Nav.Link>
-                  </Link>
-                  <Link to="/Contact">
-                     <ReactBS.Nav.Link href="#contact">Contact</ReactBS.Nav.Link>
-                  </Link>
-               </ReactBS.Nav>
-            </ReactBS.Navbar.Collapse>
-         </ReactBS.Navbar>
+         </ReactBS.Row>
+         <ReactBS.Row className="container-fluid justify-content-center">
+            <ReactBS.Navbar variant="dark">
+               <ReactBS.Navbar.Toggle aria-controls="basic-navbar-nav" />
+               <ReactBS.Navbar.Collapse id="basic-navbar-nav">
+                  <ReactBS.Nav className="medium-font-size">
+                     <Link to="/About">
+                        <ReactBS.Nav.Link href="#about">About</ReactBS.Nav.Link>
+                     </Link>
+                     <Link to="/Portfolio">
+                        <ReactBS.Nav.Link href="#portfolio">Portfolio</ReactBS.Nav.Link>
+                     </Link>
+                     <Link to="/Resume">
+                        <ReactBS.Nav.Link href="#resume">Resume</ReactBS.Nav.Link>
+                     </Link>
+                     <Link to="/Contact">
+                        <ReactBS.Nav.Link href="#contact">Contact</ReactBS.Nav.Link>
+                     </Link>
+                  </ReactBS.Nav>
+               </ReactBS.Navbar.Collapse>
+            </ReactBS.Navbar>
+         </ReactBS.Row>
       </div>
    );
 };
